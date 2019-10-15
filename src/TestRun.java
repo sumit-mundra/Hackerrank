478b80c612cb97cf1828cfb6d01b8d1a49edb879
Sumit Mundra <sumit.mundra.iitr@gmail.com>
2019-10-15 16:00:52 +0530

    removed test run

--- src/TestRun.java
+++
@@ -1,8 +0,0 @@
-public class TestRun {
-    public static void main(String[] args) {
-        String s = "asadsa";
-        System.out.println(Double.valueOf(Math.ceil(Math.log(1) / Math.log(2f))).intValue());
-        System.out.println(s.substring(s.length()));
-
-    }
-}
